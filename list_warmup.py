2d874f4ff3a78440bf15d157d95d0a38da3d7ec1
HAYBISH10 <164624151+HAYBISH10@users.noreply.github.com>
2026-09-14 22:49:53 +0300

    initial commit

---
+++ list_warmup.py
@@ -0,0 +1,12 @@
+fruits = ["apple", "banana", "orange", "mango"]
+
+print("First fruit:", fruits[0])
+print("Last fruit:", fruits[-1])
+
+fruits.append("pineapple")
+print("After adding a fruit:", fruits)
+
+fruits.remove("banana")
+print("After removing a fruit:", fruits)
+
+print("Number of fruits remaining:", len(fruits))
